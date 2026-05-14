d135edfad56f1d8376d0ac715a3a93639fe4d065
MilkMangoTea <blockbyblock@qq.com>
2026-05-14 21:09:53 +0800

    创建了电子证明生成与审批流程页面

--- apps/certificate/urls.py
+++ apps/certificate/urls.py
@@ -5,4 +5,8 @@ app_name = 'certificate'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('demo/<str:role>/', views.DemoSwitchView.as_view(), name='demo_switch'),
+    path('apply/', views.ApplyView.as_view(), name='apply'),
+    path('<int:pk>/review/', views.ReviewView.as_view(), name='review'),
+    path('<int:pk>/download/', views.DownloadView.as_view(), name='download'),
 ]
